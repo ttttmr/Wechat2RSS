2b3d6f8bc602b21419cc5a2c892ea26b48b9a0c2
ttttmr <32825326+ttttmr@users.noreply.github.com>
2025-10-13 22:33:37 +0800

    fix railway build

--- script/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install curl -y
--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt update -y > /dev/null
+apt install curl -y > /dev/null
 sleep 3s
 curl https://wechat2rss.xlab.app/release/server_amd64 -o ./server
 chmod +x ./server
